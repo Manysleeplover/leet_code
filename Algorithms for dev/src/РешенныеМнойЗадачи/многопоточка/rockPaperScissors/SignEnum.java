4a8fcd78e7f7aa1e5a0ca132f1d773025593db40
romanovio <romanovio@vtb.ru>
2024-04-06 00:23:39 +0300

    Камень-ножницы-бумага

--- Algorithms for dev/src/РешенныеМнойЗадачи/многопоточка/rockPaperScissors/SignEnum.java
+++ Algorithms for dev/src/РешенныеМнойЗадачи/многопоточка/rockPaperScissors/SignEnum.java
@@ -3,13 +3,9 @@ package РешенныеМнойЗадачи.многопоточка.rockPaperS
 enum SignEnum {
     rock(0), paper(1), scissors(2);
 
-    private final int signNumber;
+    SignEnum(int signNumber) {}
 
-    SignEnum(int signNumber) {
-        this.signNumber = signNumber;
-    }
-
-    public static SignEnum getSign(int signNumber){
+    public static SignEnum getSign(int signNumber) {
         return SignEnum.values()[signNumber];
     }
 }
